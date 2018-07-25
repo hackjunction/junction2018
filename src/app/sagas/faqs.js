053e1561ef153130faae42eb9fcf11c331fb74e6
Henrik Aarnio <hjaarnio@gmail.com>
2018-07-25 23:30:32 +0300

    forgot files

---
+++ src/app/sagas/faqs.js
@@ -0,0 +1,11 @@
+import { call, put } from 'redux-saga/effects';
+import ApiPosts from '../api/posts';
+
+export function* getFaqs() {
+  console.log('SAGA FAQ');
+  const faqs = yield call(ApiPosts.getFaqs);
+  yield put({
+    type: 'GET_FAQS_SAVE',
+    faqs: faqs.data
+  });
+}
