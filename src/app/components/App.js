87e99352a5cdcad91552f65f4b1b9a1fd4741aa2
Henrik Aarnio <hjaarnio@gmail.com>
2018-08-21 10:34:30 +0300

    add gtag

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -16,21 +16,14 @@ class App extends React.Component {
     super(props);
   }
   statistics() {
-    //google tag manager
-    (function(w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({
-        'gtm.start': new Date().getTime(),
-        event: 'gtm.js'
-      });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-WL9HBHS');
+    // google tag manager
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
 
+    gtag('config', 'UA-93845396-2');
     //hotjar
     (function(h, o, t, j, a, r) {
       h.hj =
